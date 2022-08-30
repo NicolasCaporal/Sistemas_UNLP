e98e53f4f3ea653b0bde38aee11a32f3e7fe80d4
Nico <nicolascaporal.unlp@gmail.com>
2022-08-30 15:14:50 -0300

    Create Ejercicio 2 - Practica 2.pas

---
+++ 1er_Año/TallerDeProgramacion/Imperativo/Clase2_Recursividad/Ejercicio 2 - Practica 2.pas
@@ -0,0 +1,32 @@
+{Realizar un programa que lea números hasta leer el valor 0 en imprima, para cada número leído, sus dígitos en el orden en que aparecen en el número.
+Debe implementarse un módulo recursivo que reciba el número e imprima lo pedido, Ejemplo: si se lee el valor 256 debe imprimir 2 5 6}
+
+program ejercicio2_practica2;
+
+procedure imprimirDigitos (n: integer);
+var
+	dig: integer;
+begin
+	dig := n mod 10;
+	if (dig <> 0) then
+	begin
+		n := n div 10;
+		imprimirDigitos(n);
+		writeln(dig);
+	end;
+	writeln;
+end;
+
+
+
+{PROGRAMA PRINCIPAL}
+var
+	n: integer;
+begin
+	readln(n);
+	while (n <> 0) do
+	begin
+		imprimirDigitos(n);
+		readln(n);
+	end;
+end.
