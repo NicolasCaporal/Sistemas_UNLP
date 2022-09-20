ee889bdc5b2fbe7e8049ac49bc8bc21a658f8504
Nico <nicolascaporal.unlp@gmail.com>
2022-09-19 23:03:17 -0300

    Create Ejercicio 11b - Practica 1.asm

---
+++ 1er_Año/ArquitecturaDeComputadoras/Practica 1/Ejercicio 11b - Practica 1.asm
@@ -0,0 +1,33 @@
+;11) Subrutinas de cálculo
+;b) * Escriba la subrutina RESTO que calcule el resto de la división entre 2 números positivos. Dichos números deben
+;pasarse por valor desde el programa principal a la subrutina a través de registros. El resultado debe devolverse
+;también a través de un registro por referencia. 
+
+
+ORG 1000H
+    DIVIDENDO DB 63
+    DIVISOR DB 8
+    REST DB ?
+
+ORG 3000H
+  RESTO: CMP AH, 0
+         JZ FIN_D
+         SUB AL, AH 
+         CMP AL, AH
+         JS FIN_D
+         CMP AL, 0
+         JNZ RESTO
+  FIN_D: MOV DX, 0
+         MOV DL, AL
+         RET
+
+
+ORG 2000H
+    MOV AL, DIVIDENDO
+    MOV AH, DIVISOR 
+
+    CALL RESTO
+
+    MOV REST, DL
+    HLT
+END
