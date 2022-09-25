e10c168cab0e4511c9fc74122a63ee8840f44134
Nico <110589428+NicolasCaporal@users.noreply.github.com>
2022-09-25 00:30:24 -0300

    Update Ejercicio 3 - Practica 1.java

--- 1er_Año/TallerDeProgramacion/Objetos/Clase 1/Ejercicio 3 - Practica 1.java
+++ 1er_Año/TallerDeProgramacion/Objetos/Clase 1/Ejercicio 3 - Practica 1.java
@@ -1,13 +1,13 @@
-/*3- Escriba un programa que defina una matriz de enteros de tamaño 5x5. Inicialice
-la matriz con números aleatorios entre 0 y 30.
+/*3- Escriba un programa que defina una matriz de enteros de tamaño 5x5. Inicialice la matriz con números aleatorios entre 0 y 30.
+
 Luego realice las siguientes operaciones:
 - Mostrar el contenido de la matriz en consola.
 - Calcular e informar la suma de los elementos de la fila 1
 - Generar un vector de 5 posiciones donde cada posición j contiene la suma
 de los elementos de la columna j de la matriz. Luego, imprima el vector.
-- Lea un valor entero e indique si se encuentra o no en la matriz. En caso de
-encontrarse indique su ubicación (fila y columna) en caso contrario
-imprima “No se encontró el elemento”.
+- Lea un valor entero e indique si se encuentra o no en la matriz. En caso de encontrarse indique su ubicación (fila y columna) 
+en caso contrario imprima “No se encontró el elemento”.
+
 NOTA: Dispone de un esqueleto para este programa en Ej03Matrices.java*/
 
 package tema1;
