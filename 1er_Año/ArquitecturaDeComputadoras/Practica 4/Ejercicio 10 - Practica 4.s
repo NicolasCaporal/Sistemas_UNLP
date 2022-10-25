a642b7f030f475246b3c703570a74f04821293b5
Nico <nicolascaporal.unlp@gmail.com>
2022-10-24 23:53:04 -0300

    Create Ejercicio 10 - Practica 4.s

---
+++ 1er_Año/ArquitecturaDeComputadoras/Practica 4/Ejercicio 10 - Practica 4.s
@@ -0,0 +1,24 @@
+#10) Escribir un programa que cuente la cantidad de veces que un determinado caracter aparece en una cadena de texto. Observar
+#cómo se almacenan en memoria los códigos ASCII de los caracteres (código de la letra “a” es 61H). Utilizar la instrucción lbu
+#(load byte unsigned) para cargar códigos en registros. La inicialización de los datos es la siguiente:
+
+.data
+    cadena: .asciiz "adbdcdedfdgdhdid" ; cadena a analizar
+    car: .asciiz "d" ; caracter buscado
+    cant: .word 0 ; cantidad de veces que se repite el caracter car en cadena.
+
+.code 
+    dadd r3, r0, r0 #Desplazamiento
+    lbu r2, car(r0) #Caracter a buscar
+    dadd r1, r0, r0 #Contador Resultado
+    #Cargar en r5 la longitud de la cadena ¿Cómo lo hago?
+
+    loop: lbu r4, cadena(r3) #Carga caracter actual
+        bne r2, r4, siguiente 
+        daddi r1, r1, 1
+        siguiente: daddi r5, r5, -1
+        daddi r3, r3, 2
+        bnez r5, loop
+
+sd r1, cant(r0)
+halt
