f7d56a8dadf26cd05c8a1129631bfb3d69478cef
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-03-26 23:43:23 -0300

    Ejercicio4.pas

--- 2do_Año/Fundamentos_de_Organización_de_Datos/Practica1/Ejercicio4.pas
+++ 2do_Año/Fundamentos_de_Organización_de_Datos/Practica1/Ejercicio4.pas
@@ -1,5 +1,5 @@
 { 4. Agregar al menú del programa del ejercicio 3, opciones para:
-a. Añadir uno o más empleados al final del archivo con sus datos ingresados por
+datos ingresados por
 teclado. Tener en cuenta que no se debe agregar al archivo un empleado con
 un número de empleado ya registrado (control de unicidad).
 b. Modificar edad a uno o más empleados.
@@ -28,7 +28,8 @@ begin
 
     writeln('OPCION 1: Ingresar datos de empleados. ');
     writeln('OPCION 2: Ver información de empleados. ');
-    writeln('OPCION 3: Cancelar.');
+    writeln('OPCION 3: Exportar información a .txt');
+    writeln('OPCION 4: Cancelar.');
     write('Ingrese numero: ');
     readln(respuesta);
     writeln;
@@ -198,20 +199,247 @@ begin
     close(archivoEmpleados);
 end;
 
-
-procedure ejecutarOpcionElegida (respuesta: integer; var archivoEmpleados: archivoRegistro);
+function chequearEmpleado(var archivoEmpleados: archivoRegistro; num: string):boolean;
 var 
+    return: boolean;
+    emp: empleado;
+begin
+    return := true;
+    seek(archivoEmpleados, 0);
+    while (not EOF(archivoEmpleados)) do 
+    begin
+        read(archivoEmpleados, emp);
+        if (emp.num = num) then 
+        begin
+            return := false;
+            seek(archivoEmpleados, fileSize(archivoEmpleados));
+        end; 
+    end;
+    chequearEmpleado := return;
+end;
+
+procedure agregarEmpleadosArchivo(var archivoEmpleados: archivoRegistro);
+var
+    emp: empleado;
+    cant: integer;
+
+begin
+    write('Ingrese cuantos empleados desea agregar al archivo: ');
+    readln(cant);
+    reset(archivoEmpleados);
+
+    if (cant > 0) then 
+    begin
+        while (cant > 0) do
+        begin
+            leerEmpleado(emp);
+            if (chequearEmpleado(archivoEmpleados, emp.num)) then 
+                write(archivoEmpleados, emp)
+            else 
+                writeln('Error: El empleado con numero ', emp.num , ' ya se encontraba en el archivo.');
+            cant := cant - 1;
+            writeln(cant);
+        end;
+    end 
+    else 
+        writeln('ERROR');
+    close(archivoEmpleados);
+end;
+
+procedure modificarEdad (var archivoEmpleados: archivoRegistro);
+var 
+    nro: string;
+    empActual: empleado;
+    pos: integer;
+    emp: empleado;
+begin
+
+    reset(archivoEmpleados);
+    write('Ingrese numero de empleado a modificar edad. Para finalizar ingrese "00" ');
+    readln(nro);
+    while (nro <> '00') do
+    begin
+        pos := -1;
+        seek(archivoEmpleados, 0);
+        while (not EOF(archivoEmpleados)) do 
+        begin
+            read(archivoEmpleados, empActual);
+            if (empActual.num = nro) then 
+            begin
+                emp := empActual;
+                pos := filePos(archivoEmpleados) - 1;
+                seek(archivoEmpleados, fileSize(archivoEmpleados));
+            end;
+        end; 
+        if (pos <> -1) then
+        begin
+            imprimirEmpleado(emp);
+            write('Ingrese edad: ');
+            readln(emp.edad);
+            seek(archivoEmpleados, pos);
+            write(archivoEmpleados, emp);
+        end 
+        else 
+            writeln('No se encontró al empleado número ', nro);
+
+        write('Ingrese numero de empleado a modificar edad. Para finalizar ingrese "00" ');
+        readln(nro);
+    end;
+
+    close(archivoEmpleados);
+end;
+
+procedure submenuOpcion2 (var archivoEmpleados: archivoRegistro);
+var 
+    opcion: integer;
     nombreFisico: string;
 begin
+
     write('Ingrese el nombre del archivo: ');
     readln(nombreFisico);
     assign(archivoEmpleados, nombreFisico);
     writeln; 
 
+    writeln('1. Agregar empleados');
+    writeln('2. Modificar edad de empleados');
+    write('Ingrese un numero: ');
+    readln(opcion);
+
+    if (opcion = 1) then
+    begin 
+        agregarEmpleadosArchivo(archivoEmpleados);
+    end
+    else if (opcion = 2) then 
+    begin 
+        modificarEdad(archivoEmpleados);
+    end
+    else 
+        writeln('ERROR');
+end;
+
+procedure escribirEmpleadoEnTXT (emp: empleado; var archivoTXT: text);
+begin
+    writeln(archivoTXT, emp.num);
+    writeln(archivoTXT, emp.apellido);
+    writeln(archivoTXT, emp.nombre);
+    writeln(archivoTXT, emp.edad);
+    writeln(archivoTXT, emp.DNI);
+    writeln(archivoTXT, '__________________________________');
+end;
+
+procedure generarTXTcompleto (var archivoEmpleados: archivoRegistro);
+var 
+    archivoTXT: text;
+    emp: empleado;
+begin
+    assign(archivoTXT, 'todos_empleados.txt');
+    rewrite(archivoTXT);
+    reset(archivoEmpleados);
+
+    while (not EOF(archivoEmpleados)) do 
+    begin
+        read(archivoEmpleados, emp);
+        escribirEmpleadoEnTXT(emp, archivoTXT);
+    end;
+
+    close(archivoTXT);
+    close(archivoEmpleados);
+
+end;
+
+procedure generarTXTsinDNI(var archivoEmpleados: archivoRegistro);
+var 
+    archivoTXT: text;
+    emp: empleado;
+begin
+    assign(archivoTXT, 'faltaDNIEmpleado.txt');
+    rewrite(archivoTXT);
+    reset(archivoEmpleados);
+
+    while (not EOF(archivoEmpleados)) do 
+    begin
+        read(archivoEmpleados, emp);
+        if (emp.DNI = '00') then 
+            escribirEmpleadoEnTXT(emp, archivoTXT);
+    end;
+
+    close(archivoEmpleados);
+    close(archivoTXT);
+end;
+
+procedure menuOpcion3 (var archivoEmpleados: archivoRegistro);
+var 
+    opcion: integer;
+    nombreFisico: string;
+begin
+    writeln;
+    writeln('1. Exportar a .txt toda la información');
+    writeln('2. Exportar a .txt empleados sin DNI');
+    write('Ingrese un numero: ');
+    readln(opcion);
+
+    write('Ingrese el nombre del archivo a exportar: ');
+    readln(nombreFisico);
+    assign(archivoEmpleados, nombreFisico); 
+    writeln; 
+
+    if (opcion = 1) then 
+    begin
+        generarTXTcompleto(archivoEmpleados);
+    end
+    else if (opcion = 2) then 
+    begin 
+        generarTXTsinDNI(archivoEmpleados);
+    end
+    else    
+        writeln('ERROR');
+end;
+
+procedure menuOpcion1 (var archivoEmpleados: archivoRegistro);
+var 
+    opcion: integer;
+    nombreFisico: string;
+begin
+    writeln();
+    writeln('1. Crear nuevo archivo');
+    writeln('2. Modificar un archivo ya existente');
+    write('Ingrese un numero: ');
+    readln(opcion);
+
+    if (opcion = 1) then 
+    begin
+        write('Ingrese el nombre del archivo: ');
+        readln(nombreFisico);
+        assign(archivoEmpleados, nombreFisico);
+        writeln; 
+        crearArchivo(archivoEmpleados);
+    end
+    else if (opcion = 2) then 
+        submenuOpcion2(archivoEmpleados)
+    else 
+        writeln('ERROR');
+end;
+
+procedure ejecutarOpcionElegida (respuesta: integer; var archivoEmpleados: archivoRegistro);
+var 
+    nombreFisico: string;
+begin
     if (respuesta = 1) then 
-        crearArchivo(archivoEmpleados)
-    else if (respuesta = 2) then 
+        menuOpcion1(archivoEmpleados)
+    else if (respuesta = 2) then
+    begin 
+        write('Ingrese el nombre del archivo: ');
+        readln(nombreFisico);
+        assign(archivoEmpleados, nombreFisico);
+        writeln;  
         imprimirInfo(archivoEmpleados);
+    end
+    else if (respuesta = 3) then 
+    begin 
+        menuOpcion3(archivoEmpleados);
+    end
+    else 
+        writeln('ERROR');
 end;
 
 
@@ -223,6 +451,6 @@ var
 
 begin 
     mostrarOpciones(respuesta);
-    if (respuesta <> 3) then
+    if (respuesta <> 4) then
         ejecutarOpcionElegida(respuesta, archivoEmpleados);
 end.
