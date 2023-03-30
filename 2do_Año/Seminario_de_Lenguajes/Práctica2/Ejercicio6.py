a7f08e6ab953eddaf0a6ed0f577e8412e6ccf640
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-03-30 17:30:43 -0300

    Create Ejercicio6.py

---
+++ 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio6.py
@@ -0,0 +1,9 @@
+# 6. Retomando el código visto en la teoría (ver PDF), que informaba si una palabra contenía la letra a en una palabra ingresada:
+# Modificarlo para que ahora imprima si contiene la letra a y también la letra n
+
+palabra = input("Ingresá una palabra: ")
+
+if "a" and "n" in palabra:
+    print("Hay letras a y n")
+else:
+    print("No hay letras a ni n ")
