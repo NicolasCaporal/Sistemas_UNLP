b830e12c2027c0c51314c085a6bd3a8e8b3dff81
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-03-30 17:19:45 -0300

    Fix readme

--- 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio2.py
+++ 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio2.py
@@ -41,7 +41,16 @@ help us stay on top of new and old issues
 develop tutorials, presentations, and other educational materials
 maintain and improve our website
 develop graphic design for our brand assets and promotional materials
-translate website content"""
+translate website content
+help with outreach and onboard new contributors
+write grant proposals and help with other fundraising efforts
+For more information about the ways you can contribute to NumPy, visit our website. If you’re unsure where to start or how your skills fit in, reach out! You can ask on the mailing list or here, on GitHub, by opening a new issue or leaving a comment on a relevant issue that is already open.
+
+Our preferred channels of communication are all public, but if you’d like to speak to us in private first, contact our community coordinators at numpy-team@googlegroups.com or on Slack (write numpy-team@googlegroups.com for an invitation).
+
+We also have a biweekly community call, details of which are announced on the mailing list. You are very welcome to join.
+
+If you are new to contributing to open source, this guide helps explain why, what, and how to successfully get involved."""
 
 words = readme.lower().split()
 max = Counter(words).most_common(1)[0][0]
