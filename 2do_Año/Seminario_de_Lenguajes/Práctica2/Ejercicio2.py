d776af460215d4d180c9db8b9124cbebabf9e7fc
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-03-29 23:51:03 -0300

    Create

---
+++ 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio2.py
@@ -0,0 +1,48 @@
+# 2. Indique la palabra que aparece mayor cantidad de veces en el texto del README.md de numpy.
+# Copie y pegue el texto en una varible.
+# Recordemos algunas funciones de string:
+# • lower
+# • split
+# Investigue el módulo Counter para simplificar la resolución.
+from collections import Counter
+
+readme = """NumPy is the fundamental package for scientific computing with Python.
+
+Website: https://www.numpy.org
+Documentation: https://numpy.org/doc
+Mailing list: https://mail.python.org/mailman/listinfo/numpy-discussion
+Source code: https://github.com/numpy/numpy
+Contributing: https://www.numpy.org/devdocs/dev/index.html
+Bug reports: https://github.com/numpy/numpy/issues
+Report a security vulnerability: https://tidelift.com/docs/security            
+It provides:
+
+a powerful N-dimensional array object
+sophisticated (broadcasting) functions
+tools for integrating C/C++ and Fortran code
+useful linear algebra, Fourier transform, and random number capabilities
+Testing:
+
+NumPy requires pytest and hypothesis. Tests can then be run after installation with:
+
+python -c "import numpy, sys; sys.exit(numpy.test() is False)"
+Code of Conduct
+NumPy is a community-driven open source project developed by a diverse group of contributors. The NumPy leadership has made a strong commitment to creating an open, inclusive, and positive community. Please read the NumPy Code of Conduct for guidance on how to interact with others in a way that makes our community thrive.
+
+Call for Contributions
+The NumPy project welcomes your expertise and enthusiasm!
+
+Small improvements or fixes are always appreciated. If you are considering larger contributions to the source code, please contact us through the mailing list first.
+
+Writing code isn’t the only way to contribute to NumPy. You can also:
+
+review pull requests
+help us stay on top of new and old issues
+develop tutorials, presentations, and other educational materials
+maintain and improve our website
+develop graphic design for our brand assets and promotional materials
+translate website content"""
+
+words = readme.lower().split()
+max = Counter(words).most_common(1)[0][0]
+print(f'Palabra más repetida: {max}')
