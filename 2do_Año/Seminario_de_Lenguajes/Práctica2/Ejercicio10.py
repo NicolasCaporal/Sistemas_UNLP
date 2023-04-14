1fb44312d4a6375175791bbd28bf3e60f2ecefd0
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-04-14 17:10:50 -0300

    Update Ejercicio10.py

--- 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio10.py
+++ 2do_Año/Seminario_de_Lenguajes/Práctica2/Ejercicio10.py
@@ -1,36 +1,58 @@
 # 10. Dada una lista de nombres de estudiantes y dos listas con sus notas en un curso, escriba un
 # programa que manipule dichas estructuras de datos para poder resolver los siguientes puntos:
-# A. Generar una estructura todas las notas sin que se repitan, sin guardar los nombres de los
-# estudiantes.
+# A. Generar una estructura con todas las notas relacionando el nombre del estudiante con las
+# notas. Utilizar esta estructura para la resolución de los siguientes items.
 # B. Calcular el promedio de notas de cada estudiante.
 # C. Calcular el promedio general del curso.
 # D. Identificar al estudiante con la nota promedio más alta.
 # E. Identificar al estudiante con la nota más baja.
+
 # Nota:
-# • Las 3 estructuras están ordenadas de forma que los elementos en la misma posición corresponden
-# a un mismo alumno.
-# • Realizar funciones con cada item
+#   • Las 3 estructuras están ordenadas de forma que los elementos en la misma posición corresponden a un mismo alumno.
+#   • Realizar funciones con cada item
 
-def generarListaSinRepeticiones(cadena):
-    return set(cadena)
+def generarDiccionarioNotas(listaNombres, notas_1, notas_2):
+    """ Esta función recibe 3 parametros. Una lista de nombres, y dos listas de notas
+        Retorna un diccionario de la forma {Nombre: (nota1, nota2)}"""
+    
+    notas = list(zip(notas_1, notas_2))
+    return dict(zip(listaNombres, notas))
 
-def calcularPromedios(notas_1, notas_2):
-    notasXEstudiante = list(zip(notas_1, notas_2))
-    promedios = list(map(lambda notas: sum(notas)/2, notasXEstudiante))
+
+def generarDiccionarioPromedios(estudiantes):
+    """ Esta funcion recibe un diccionario de la forma {Nombre: (nota1, nota2)}
+    Calcula el promedio de las 2 notas de cada estudiante.
+    Retorna un diccionario de la forma {Nombre: promedio}"""
+
+    promedios = {nombre: sum(notas)/len(notas) for nombre, notas in estudiantes.items()}
     return promedios
 
-def calcularPromedioGeneral(notas):
-    return (sum(notas)/len(notas))
 
-def estudianteMejorPromedio(nombres, promedios):
-    mejorPromedio = max(promedios)
-    i = promedios.index(mejorPromedio)
-    return nombres[i] 
+def calcularPromedioGeneral(promedios):
+    """ Esta funcion recibe un diccionario del tipo {Nombre: promedio}
+    Retorna un float redondeado a 2 decimales con el promedio general"""
 
-def estudiantePeorNota(nombres, notas):
-    peorNota = min(notas)
-    i = notas.index(peorNota)
-    return nombres[i]
+    suma = sum(promedios.values())
+    cantidadAlumnos = len(promedios)
+    promedio = suma / cantidadAlumnos
+
+    return round(promedio, 2)
+
+
+def estudianteMejorPromedio(promedios):
+    """ Esta funcion recibe un diccionario del tipo {Nombre: promedio}
+    Retorna el Nombre del estudiante con el promedio más alto"""
+
+    mejor_promedio = max(promedios.items(), key = lambda e: e[1])
+    return mejor_promedio[0]
+
+
+def estudianteNotaMasBaja(notas):
+    """ Esta función recibe un diccionario del tipo {Nombre: (nota1, nota2)
+    Retorna el Nombre del estudiante que entre alguna de sus dos notas, tenga la nota más baja"""
+    peor_nota = min(notas, key=lambda e: min(notas[e]))
+    return peor_nota
+
 
 nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', 'CESAR', 'David',
 'Diego', 'Dolores', 'DYLAN', 'ELIANA', 'Emanuel', 'Fabián', 'Facundo', 'Francsica',
@@ -39,31 +61,27 @@ nombres = ''' 'Agustin', 'Alan', 'Andrés', 'Ariadna', 'Bautista', 'CAROLINA', '
 'Luis', 'Marcos', 'María', 'MATEO', 'Matias', 'Nicolás', 'Nancy', 'Noelia', 'Pablo',
 'Priscila', 'Sabrina', 'Tomás', 'Ulises', 'Yanina' '''
 
-notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69, 12,
-13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44, 85, 73, 37, 42, 95, 18,
-74, 60, 9, 65, 93, 63, 74]
+notas_1 = [81, 60, 72, 24, 15, 91, 12, 70, 29, 42, 16, 3, 35, 67, 10, 57, 11, 69,
+12, 77, 13, 86, 48, 65, 51, 41, 87, 43, 10, 87, 91, 15, 44,
+85, 73, 37, 42, 95, 18, 7, 74, 60, 9, 65, 93, 63, 74]
 
-notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37, 64,
-87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73, 95, 19, 47, 15,
-39, 15, 74, 33, 57, 10]
+notas_2 = [30, 95, 28, 84, 84, 43, 66, 51, 4, 11, 58, 10, 13, 34, 96, 71, 86, 37,
+64, 13, 8, 87, 14, 14, 49, 27, 55, 69, 77, 59, 57, 40, 96, 24, 30, 73,
+95, 19, 47, 15, 31, 39, 15, 74, 33, 57, 10]
 
-todasNotas = generarListaSinRepeticiones(notas_1 + notas_2) 
+#A
+nombres = nombres.replace("'", " ") # Limpio el string de '
+listaNombres = [nombre.strip() for nombre in nombres.split(',')] # Creo una lista de nombres, y eliminio los espacios
+diccionario_notas_estudiantes = generarDiccionarioNotas(listaNombres, notas_1, notas_2)
 
-promedios = calcularPromedios(notas_1, notas_2)
-print(promedios)
+#B
+diccionario_promedios = generarDiccionarioPromedios(diccionario_notas_estudiantes)
 
-promedioGeneral = calcularPromedioGeneral(notas_1 + notas_2)
-print(f"Promedio general: {promedioGeneral}")
+#C
+print(f'Promedio general del curso: {calcularPromedioGeneral(diccionario_promedios)}')
 
+#D
+print(f'Estudiante con mejor promedio: {estudianteMejorPromedio(diccionario_promedios)}')
 
-
-
-nombres = nombres.replace("'", " ")
-listaNombres = nombres.split(',')
-listaNombres = [x.strip() for x in listaNombres]
-
-nombreMejorPromedio = estudianteMejorPromedio(listaNombres, promedios)
-print(f"Alumno con mejor promedio: {nombreMejorPromedio}")
-
-nombrePeorNota = estudiantePeorNota(listaNombres, notas_1 + notas_2)
-print(f"Alumno peor nota: {nombrePeorNota}")
+#E
+print(f'Estudiante con la nota mas baja: {estudianteNotaMasBaja(diccionario_notas_estudiantes)}')
