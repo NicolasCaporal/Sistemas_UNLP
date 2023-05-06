cf23e7c54c3cd41ff42a97f26cab18a43a356645
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-05-06 00:55:55 -0300

    Parcial

---
+++ 2do_Año/Algoritmos_y_Estructuras_de_Datos/PracticaParcial1/Parcial4.java
@@ -0,0 +1,61 @@
+/* Implemente en la clase Parcial el metodo resolver que recibe un arbol binario de enteros positivos y un
+ numero entero y devuelve un camino que cumple con la siguiente condicion: 
+ la cantidad de numeros pares que contenga dicho camino debe ser mayor o igual al parametro "min".
+ Si existen varios caminos que cumplen la condicion, el metodo debe devolver el primer camino que encuentre.
+ Firma: ListaGenerica<Integer> resolver(ArbolBinario<Integer> ab, int min)*/
+
+package practica4;
+
+public class Parcial4 {
+	
+	    public  ListaGenerica<Integer> resolver (ArbolBinario<Integer> ab, int min){
+	        ListaGenerica<Integer> lista = new ListaEnlazadaGenerica<Integer>();
+	        resolverP(ab, min, lista);
+	        return lista;
+	    }
+
+	    private  Boolean resolverP (ArbolBinario<Integer> arbol, int min, ListaGenerica<Integer> lista){
+	        
+	        int dato = arbol.getDato();
+	        lista.agregarFinal(dato);
+
+	         if (arbol.esHoja()){
+	            boolean rta;
+	            if (min <= 0)
+	                rta = true;
+	            else 
+	                rta = false;
+	            return rta;
+	         }
+
+	        int par = 0;
+	        if (dato % 2 == 0){
+	            par = 1;
+	        }
+
+	        boolean resultado = true;
+	        boolean seguir = true;
+	        if (arbol.tieneHijoIzquierdo()){
+	            resultado = resolverP(arbol.getHijoIzquierdo(), min-par, lista);
+
+	            if (!resultado){
+	                lista.eliminarEn(lista.tamanio());
+	            } else {
+	                seguir = false;
+	            }
+
+	        }
+
+	        if ((arbol.tieneHijoDerecho()) && (seguir)){
+	            resultado = resolverP(arbol.getHijoDerecho(), min-par, lista);
+
+	            if (!resultado){
+	                lista.eliminarEn(lista.tamanio());
+	            }
+	        }
+
+	        return resultado;
+        }
+	    
+}
+
