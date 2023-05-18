f1606a8b5bf3678c5694df58cbc1babb52bef198
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-05-17 23:09:32 -0300

    Update Ejercicio8.pas

--- 2do_Año/Fundamentos_de_Organización_de_Datos/Practica2/Ejercicio8.pas
+++ 2do_Año/Fundamentos_de_Organización_de_Datos/Practica2/Ejercicio8.pas
@@ -1,10 +1,8 @@
 { 8. Se cuenta con un archivo que posee información de las ventas que realiza una empresa a
 los diferentes clientes. Se necesita obtener un reporte con las ventas organizadas por
 cliente. Para ello, se deberá informar por pantalla: los datos personales del cliente, el total
-mensual (mes por mes cuánto compró) y finalmente el monto total comprado en el año por el
-cliente.
-Además, al finalizar el reporte, se debe informar el monto total de ventas obtenido por la
-empresa.
+mensual (mes por mes cuánto compró) y finalmente el monto total comprado en el año por el cliente.
+Además, al finalizar el reporte, se debe informar el monto total de ventas obtenido por la empresa.
 El formato del archivo maestro está dado por: cliente (cod cliente, nombre y apellido), año,
 mes, día y monto de la venta.
 El orden del archivo está dado por: cod cliente, año y mes.
