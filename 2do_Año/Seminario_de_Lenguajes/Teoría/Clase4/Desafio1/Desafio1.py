419c19ecd70ed606e18a0364d03ff3bf3ea855f0
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-05-18 22:50:24 -0300

    Update Desafio1.py

--- 2do_Año/Seminario_de_Lenguajes/Teoría/Clase4/Desafio1/Desafio1.py
+++ 2do_Año/Seminario_de_Lenguajes/Teoría/Clase4/Desafio1/Desafio1.py
@@ -1,4 +1,4 @@
-# Desafío
+# Desafío 
 # • Dado el conjunto de datos con series y películas de Netflix, queremos:
 # 1- Guardar en otro archivo, en formato json, las peliculas realizadas por más de un país.
 # 2- Imprimir los cinco (5) países con más producciones en Netflix, durante el año 2019.
