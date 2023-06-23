1a24257151ec0fc1260da229e0897daa5bb5a8b9
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-06-23 13:50:55 -0300

    Update Mapa.java

--- 2do_Año/Algoritmos_y_Estructuras_de_Datos/Practica6/Ejercicio05/Mapa.java
+++ 2do_Año/Algoritmos_y_Estructuras_de_Datos/Practica6/Ejercicio05/Mapa.java
@@ -7,6 +7,7 @@ public class Mapa {
 		this.mapa = grafo;
 	}
 	
+	
 	/* El método devolverCamino (String ciudad1, String ciudad2): ListaGenerica<String> //
 	Retorna la lista de ciudades que se deben atravesar para ir de ciudad1 a ciudad2 en caso que se pueda
 	llegar, si no retorna la lista vacía. (Sin tener en cuenta el combustible). */
@@ -25,7 +26,7 @@ public class Mapa {
 
 			for (int i = 1; i <= mapa.listaDeVertices().tamanio(); i++) {
 				if (!marca[i] && !encontro){
-					encontro = dfs(verticeCiudad1.getPosicion(), mapa, lista, marca, ciudad2) ;
+					encontro = dfs(verticesCiudades[0], mapa, lista, marca, ciudad2) ;
 				}
 			}
 			
@@ -44,6 +45,7 @@ public class Mapa {
 		return lista;
 	}	
 	
+	
 	private int[] buscarCiudades(ListaGenerica<Vertice<String>> vertices, String ciudad, String ciudad2){
 		vertices.comenzar();
 
@@ -54,15 +56,15 @@ public class Mapa {
 		while(!(vertices.fin())) {
 			ciudadActual = vertices.proximo();
 			if (ciudadActual.dato().equals(ciudad)){
-				rta[0] = ciudadActual;
+				rta[0] = ciudadActual.getPosicion();
 				nro += 1;
-			} else if (cciudadActual.dato().equals(ciudad2)){
-				rta[1] = ciudadActual;
+			} else if (ciudadActual.dato().equals(ciudad2)){
+				rta[1] = ciudadActual.getPosicion();
 				nro += 1;
 			}
 		}
 
-		if (nro = 2)
+		if (nro == 2)
 			return rta;
 		else
 			return null;
@@ -115,10 +117,25 @@ public class Mapa {
 			boolean encontro = false;
 			boolean[] marca = new boolean[mapa.listaDeVertices().tamanio()+1]; 
 			
+			ListaGenerica<Vertice<String>> vertices = mapa.listaDeVertices();
+			
+			vertices.comenzar();
+			Vertice<String> ciudadActual = vertices.proximo();
+			while (!vertices.fin()) {
+				ciudades.comenzar();
+				while(!ciudades.fin()) {
+					if (ciudadActual.dato().equals(ciudades.proximo())){
+						marca[ciudadActual.getPosicion()] = true;
+					}
+				}
+				ciudadActual = vertices.proximo();
+			}
+			
+			
 			lista.agregarFinal(ciudad1);
-			for (int i = 1; i <= mapa.listaDeVertices().tamanio(); i++) {
+			for (int i = 1; i <= vertices.tamanio(); i++) {
 				if ((!marca[i]) && (!encontro)) {				
-					encontro = dfsExceptuando(verticeCiudad1.getPosicion(), mapa, lista, marca, ciudad2, ciudades);
+					encontro = dfs(verticesCiudades[0], mapa, lista, marca, ciudad2);
 				}
 			}
 			if (encontro) {
@@ -136,138 +153,99 @@ public class Mapa {
 		return lista;
 	}
 	
-	private boolean dfsExceptuando(int i, Grafo<String> mapa, ListaGenerica<String> lista, boolean[] marca, String ciudad, ListaGenerica<String> ciudadesAEvitar) {
-		
-		ListaGenerica<Vertice<String>> vertices = mapa.listaDeVertices();
-		Vertice<String> ciudadActual = vertices.elemento(i);
-		boolean encontro = false;
-		
-		if (ciudadActual.dato().equals(ciudad)) {
-			encontro = true;
-			
-		} else {
-			
-			ciudadesAEvitar.comenzar();
-			while(!ciudadesAEvitar.fin()) {
-				if (ciudadActual.dato().equals(ciudadesAEvitar.proximo())){
-					return false;
-				}
-			}
-			
-			ListaGenerica<Arista<String>> adyacentes = mapa.listaDeAdyacentes(ciudadActual);
-			
-			adyacentes.comenzar();
-			
-			while (!adyacentes.fin() && !encontro) {
-				Vertice<String> destino = adyacentes.proximo().verticeDestino();
-				int j = destino.getPosicion();
-				
-				if (!marca[j]) {
-					lista.agregarFinal(destino.dato());
-					encontro = dfsExceptuando(j, mapa, lista, marca, ciudad, ciudadesAEvitar);
-					if (!lista.elemento(lista.tamanio()).equals(ciudad)) {
-						lista.eliminarEn(lista.tamanio());
-					}
-				}
-			}
-			
-		}
-		return encontro;
-	}
-	
 	
 	/* El método caminoMasCorto(String ciudad1, String ciudad2): ListaGenerica<String> //
 	Retorna la lista de ciudades que forman el camino más corto para llegar de ciudad1 a ciudad2, si no
 	existe camino retorna la lista vacía. (Las rutas poseen la distancia). (Sin tener en cuenta el
 	combustible) */
 	public ListaGenerica<String> caminoMasCorto (String ciudad1, String ciudad2){
+		ListaGenerica<String> camActual = new ListaEnlazadaGenerica<String>();
+		ListaGenerica<String> camMinimo = new ListaEnlazadaGenerica<String>();
+
 		
+		int[] verticesCiudades = buscarCiudades(mapa.listaDeVertices(), ciudad1, ciudad2);
 				
-		int[] verticesCiudades = buscarCiudades(mapa.listaDeVertices(), ciudad1, ciudad2);	
-
-		int costo[] = new int[mapa.listaDeVertices().tamanio()+1]; //D
-		int anterior[] = new int[mapa.listaDeVertices().tamanio()+1]; //P
-		int iCiudad1 = verticeCiudad1.getPosicion();
-		boolean[] marca = new boolean[mapa.listaDeVertices().tamanio()+1];
-		
-		if (verticesCiudades != null) {		
-			dijkstra(iCiudad1, costo, anterior, marca);
-		} else {
-			System.out.println("Alguna de las 2 ciudades no existe");
-		}
-		
-		ListaGenerica<String> lista = new ListaEnlazadaGenerica<String>();
-		int i = verticeCiudad2.getPosicion();
-		if (costo[i] != 9999) {
+		if (verticesCiudades != null) {
+			boolean encontro = false;
+			boolean[] marca = new boolean[mapa.listaDeVertices().tamanio() + 1];
 			
-			while(costo[i] != 0) {
-				lista.agregarInicio(mapa.listaDeVertices().elemento(i).dato());
-				i = anterior[i];
+			ListaGenerica<Vertice<String>> vertices = mapa.listaDeVertices();
+			
+			
+			vertices.comenzar();
+			camActual.agregarFinal(ciudad1);
+			for (int i = 1; i<= vertices.tamanio(); i++) {
+				if (!marca[i] && (!encontro)) {
+					dfsMinimo(verticesCiudades[0], mapa, camActual, camMinimo, 0, Integer.MAX_VALUE, marca, ciudad2);
+				}
 			}
-			lista.agregarInicio(ciudad1);
-			System.out.println("Proceso exitoso");
-
+			
+			if (camActual.tamanio() == 1) {
+				camActual.eliminarEn(camActual.tamanio());
+				System.out.println("No se puede llegar desde " + ciudad1 + " hasta " +  ciudad2);
+				return camActual;
+				
+			} else {
+				System.out.println("Proceso existoso");
+			}
+			
 		} else {
-			System.out.println("No se puede llegar desde " + ciudad1 + " hasta " + ciudad2);
+			System.out.println("Alguna de las 2 ciuadades no existe");
 		}
 		
-		return lista;
+		return camMinimo;
 	}
 	
-	public void dijkstra (int ciudadInicio, int[] costo, int[] anterior, boolean[] marca) {
+	
+	private int dfsMinimo (int i, Grafo<String> mapa, ListaGenerica<String> camActual, ListaGenerica<String> camMinimo, int valorActual, int valorMinimo, boolean[] marca, String ciudad2) {
+		marca[i] = true;
+		Vertice<String> vertice = mapa.listaDeVertices().elemento(i);
 		
-		// Inicializo la tabla
-		for(int i = 0; i <= mapa.listaDeVertices().tamanio(); i++) {
-			costo[i] = 9999;
-			anterior[i] = 0;
-		}
-		costo[ciudadInicio] = 0;
-		
-		// Procesamiento
-		for (int h = 1; h <= mapa.listaDeVertices().tamanio(); h++) {
-			
-			int min = Integer.MAX_VALUE;
-			int iMin = -1;
-			for (int j = 1; j <= mapa.listaDeVertices().tamanio(); j++) {
-				if (costo[j] < min && !marca[j]) {
-					min = costo[j];
-					iMin = j;
-				}
-				if (iMin == 0)
-					System.out.println("I MIN ES 0 !!!!!!!");
+
+		if (vertice.dato().equals(ciudad2)) {
+
+			if (valorActual < valorMinimo) {
+				ListaGenerica<String> nuevo = camActual.clonar();
+				camMinimo = nuevo;
+				valorMinimo = valorActual;
+				marca[i] = false;
 			}
 			
-			marca[iMin] = true;
-			Vertice<String> vertice = mapa.listaDeVertices().elemento(iMin);
+		} else {
 			
 			ListaGenerica<Arista<String>> adyacentes = mapa.listaDeAdyacentes(vertice);
 			
-			int costoActual;
-			int costoAvance;
-			int costoDestino;
-					
 			adyacentes.comenzar();
-			while(!adyacentes.fin()) {
-				
+			int sumaActual = valorActual;
+			int minimoLocal = valorMinimo;
+
+			while (!adyacentes.fin()) {
+
 				Arista<String> arista = adyacentes.proximo();
+				sumaActual = valorActual + arista.peso();
+
 				Vertice<String> destino = arista.verticeDestino();
-				int indiceDestino = destino.getPosicion();
 				
-				if (!marca[indiceDestino]) {
-					 costoActual = costo[iMin];
-					 costoAvance = costoActual + arista.peso(); 
-					 costoDestino = costo[indiceDestino];
-					 
-					 if (costoAvance < costoDestino) {
-						 costo[indiceDestino] = costoAvance;
-						 anterior[indiceDestino] = iMin;
-					 }
-					 
-				}
-				 
+				int j = destino.getPosicion();
+				camActual.agregarFinal(destino.dato());
+				
+				if (!marca[j])
+					minimoLocal = dfsMinimo(j, mapa, camActual, camMinimo, sumaActual, valorMinimo, marca, ciudad2);
+			
+
+				if (minimoLocal < valorMinimo)
+					valorMinimo = minimoLocal;
+				
+				camActual.eliminarEn(camActual.tamanio());
+			
+				sumaActual = sumaActual - arista.peso();
 			}
+			
 		}
-		
-		
+				
+		return valorMinimo;
 	}
+			
 }
+	
+	
