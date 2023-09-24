306a56d3be8d7df497481bfe57e72107d69d2c82
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-09-23 23:44:30 -0300

    Update Ejercicio16.sh

--- 2do_Año/Introducción_a_los_Sistemas_Operativos/Practica3/Scripts.sh/Ejercicio16.sh
+++ 2do_Año/Introducción_a_los_Sistemas_Operativos/Practica3/Scripts.sh/Ejercicio16.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Realizar un script que reciba como parámetro una extensión y haga un reporte con 2
+# 16. Realizar un script que reciba como parámetro una extensión y haga un reporte con 2
 # columnas, el nombre de usuario y la cantidad de archivos que posee con esa extensión.
 # Se debe guardar el resultado en un archivo llamado reporte.txt
 
