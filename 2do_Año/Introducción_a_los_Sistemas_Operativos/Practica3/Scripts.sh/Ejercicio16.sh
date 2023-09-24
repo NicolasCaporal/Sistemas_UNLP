5ab0efe15ec1b3d96d27d3b65f6dd8f4ed5a9ced
NicolasCaporal <nicolascaporal.unlp@gmail.com>
2023-09-23 23:41:09 -0300

    Create Ejercicio16.sh

---
+++ 2do_Año/Introducción_a_los_Sistemas_Operativos/Practica3/Scripts.sh/Ejercicio16.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Realizar un script que reciba como parámetro una extensión y haga un reporte con 2
+# columnas, el nombre de usuario y la cantidad de archivos que posee con esa extensión.
+# Se debe guardar el resultado en un archivo llamado reporte.txt
+
+if [ $# -ne 1 ]; then
+        echo "ERROR: Debe enviar 1 parámetro con el parametro"
+        exit 1
+fi
+
+ext=$1
+echo > reporte.txt
+
+for user_data in $(cat /etc/passwd | cut -d: -f  1,6); do
+        user=$(echo $user_data | cut -d: -f 1)
+        home=$(echo $user_data | cut -d: -f 2)
+        
+        if [ -d $home ]; then
+                cant=$(find $home -name "*.$ext" | wc -l)
+        fi
+
+        echo "$user: $cant" >> reporte.txt
+done
+
+exit 0
